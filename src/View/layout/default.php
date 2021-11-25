c2c3db3dc9a4c78e85dfc6f87a9545af9120fc99
Stebou <stebou8mendy@gmail.com>
2021-11-25 18:59:50 +0100

    Add home-page wip

--- src/View/layout/default.php
+++ src/View/layout/default.php
@@ -16,9 +16,8 @@
 
 <body>
     <?php include ROOT . '/src/View/parts/_navbar.php'; ?>
-    <div class="container">
-        <?= $content ?>
-    </div>
+    <?= $content ?>
+
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- src/View/main/index.php
+++ src/View/main/index.php
@@ -1,1 +1,87 @@
-<h1>Page d'accueil</h1>
+<div class="home-page">
+    <div class="banner">
+        <h1>Bienvenue sur Donkey Car</h1>
+        <form>
+            <div class="wrapper">
+                <div class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success search" type="submit">Search</button>
+                </div>
+                <div class="container-datepicker">
+                    <input type="date" name="" id="">
+                    <input type="date" name="" id="">
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="container container-one">
+        <div class="row">
+            <div class="col-md-4 box">
+                <img class="img-fluid" src="https://via.placeholder.com/468x200" alt="">
+                <div class="card__description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus iste esse nihil dolorum illo numquam error, dolores, officiis corporis voluptatum accusantium et excepturi, eaque laboriosam possimus distinctio molestias saepe nemo.
+                </div>
+            </div>
+            <div class="col-md-4 box">
+                <img class="img-fluid" src="https://via.placeholder.com/468x200" alt="">
+                <div class="card__description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus iste esse nihil dolorum illo numquam error, dolores, officiis corporis voluptatum accusantium et excepturi, eaque laboriosam possimus distinctio molestias saepe nemo.
+                </div>
+            </div>
+            <div class="col-md-4 box">
+                <img class="img-fluid" src="https://via.placeholder.com/468x200" alt="">
+                <div class="card__description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus iste esse nihil dolorum illo numquam error, dolores, officiis corporis voluptatum accusantium et excepturi, eaque laboriosam possimus distinctio molestias saepe nemo.
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <h2>Lorem ipsum dolor sit amet consectetur</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-fluid" src="https://via.placeholder.com/468x200" alt="">
+            </div>
+            <div class="col-md-6">
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container-fluid">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://via.placeholder.com/368x200" class="d-block w-100 image-fluid" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://via.placeholder.com/368x200" class="d-block w-100 image-fluid" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://via.placeholder.com/368x200" class="d-block w-100 image-fluid" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</div>
