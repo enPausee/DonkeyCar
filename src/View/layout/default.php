1d145b77f92f3ad3cb6e78aa5d95bcd2b9875566
Stebou <stebou8mendy@gmail.com>
2021-11-26 10:55:34 +0100

    Update home-page, controller and style.css, add temporary images

--- src/View/layout/default.php
+++ src/View/layout/default.php
@@ -7,6 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Dynamic addition of the meta description-->
     <meta name="description" content="<?= $this->description ?>">
+
+    <!--font-awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -18,7 +22,8 @@
     <?php include ROOT . '/src/View/parts/_navbar.php'; ?>
     <?= $content ?>
 
-
+    <!-- Jquery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="/js/script.js"></script>
--- src/View/main/index.php
+++ src/View/main/index.php
@@ -1,15 +1,16 @@
 <div class="home-page">
     <div class="banner">
         <h1>Bienvenue sur Donkey Car</h1>
+        <?php include(ROOT . '/src/View/parts/_showErrorsForm.php') ?>
         <form>
             <div class="wrapper">
                 <div class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success search" type="submit">Search</button>
+                    <button class="btn btn-outline-success search" name="search" type="submit">Search</button>
                 </div>
                 <div class="container-datepicker">
-                    <input type="date" name="" id="">
-                    <input type="date" name="" id="">
+                    <input type="date" name="fromDate" id="fromDate">
+                    <input type="date" name="toDate" id="toDate">
                 </div>
             </div>
         </form>
@@ -56,7 +57,7 @@
         </div>
     </div>
     <hr>
-    <div class="container-fluid">
+    <div class="container">
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -65,13 +66,13 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="https://via.placeholder.com/368x200" class="d-block w-100 image-fluid" alt="...">
+                    <img src="picture/bugatti-electric.png" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="https://via.placeholder.com/368x200" class="d-block w-100 image-fluid" alt="...">
+                    <img src="picture/chiron.png" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="https://via.placeholder.com/368x200" class="d-block w-100 image-fluid" alt="...">
+                    <img src="picture/mansory-bugatti-chiron.png" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -84,4 +85,40 @@
             </button>
         </div>
     </div>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card opinion-grp">
+                    <div class="card-body">
+                        <p class="opinion"><i class="fas fa-user-tie fa-3x"></i></p>
+                        <p class="opinion-firstname">Stephen</p>
+                        <p>Un super service.<br>
+                            Je vous recommande ce site</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card opinion-grp">
+                    <div class="card-body">
+                        <p class="opinion"><i class="fas fa-user-secret fa-3x"></i></p>
+                        <p class="opinion-firstname">James</p>
+                        <p>Un super service.<br>
+                            Je vous recommande ce site</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card opinion-grp">
+                    <div class="card-body">
+                        <p class="opinion"><i class="fas fa-user-nurse fa-3x"></i></p>
+                        <p class="opinion-firstname">Julia</p>
+                        <p>Un super service.<br>
+                            Je vous recommande ce site</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
 </div>
