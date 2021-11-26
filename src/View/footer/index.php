386b3587d1de4fa726605e1c94a16412100bc89e
smectaz <90684643+smectaz@users.noreply.github.com>
2021-11-26 12:42:49 +0100

    création des cgv

--- src/View/footer/index.php
+++
@@ -1,9 +0,0 @@
-<h1><?=$this->h1?></h1>
-<footer>
-    <ul>
-        <li><a href="#">C.G.V</a></li>
-        <li><a href="#">F.A.Q</a></li>
-        <li><a href="#">Nous Contacter</a></li>
-        <li><a href="#"></a></li>
-    </ul>
-</footer>
