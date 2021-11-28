8c28095e827825e5e8fe80c7bb59e9043bbc2111
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2021-11-28 19:01:54 +0100

    fixed the label animation

--- src/View/user/register.php
+++ src/View/user/register.php
@@ -8,42 +8,42 @@
 
   <div class="register-first-name">
     <div class="user-box">
-      <input type="text" id="first_name" name="first_name" value="<?= App\Service\Validator::get_input_data('first_name'); ?>">
+      <input type="text" id="first_name" name="first_name" value="<?= App\Service\Validator::get_input_data('first_name'); ?>" onchange="this.setAttribute('value', this.value);">
       <label for="first_name">FirstName</label>
     </div>
   </div>
 
   <div class="register-last-name">
     <div class="user-box">
-      <input type="text" id="last_name" name="last_name" value="<?= App\Service\Validator::get_input_data('last_name'); ?>">
+      <input type="text" id="last_name" name="last_name" value="<?= App\Service\Validator::get_input_data('last_name');?>" onchange="this.setAttribute('value', this.value);">
       <label for="last_name">LastName</label>
     </div>
   </div>
 
   <div class="register-birthday">
     <div class="user-box">
-      <input type="date" id="birth_date" name="birth_date" value="<?= App\Service\Validator::get_input_data('birth_date'); ?>">
+      <input type="date" id="birth_date" name="birth_date" value="<?= App\Service\Validator::get_input_data('birth_date'); ?>" required>
       <label for="birth_date">BirthDate</label>
     </div>
   </div>
 
   <div class="register-phone">
     <div class="user-box">
-      <input type="text" id="phone" name="phone" value="<?= App\Service\Validator::get_input_data('phone'); ?>">
+      <input type="text" id="phone" name="phone" value="<?= App\Service\Validator::get_input_data('phone'); ?>" onchange="this.setAttribute('value', this.value);">
       <label for="phone">Phone</label>
     </div>
   </div>
 
   <div class="register-email">
     <div class="user-box">
-      <input type="text" id="email" name="email" value="<?= App\Service\Validator::get_input_data('email'); ?>">
+      <input type="text" id="email" name="email" value="<?= App\Service\Validator::get_input_data('email'); ?>" onchange="this.setAttribute('value', this.value);">
       <label for="email">Email</label>
     </div>
   </div>
 
   <div class="register-mdp-1">
     <div class="user-box">
-      <input type="password" id="password" name="password" value="<?= App\Service\Validator::get_input_data('password'); ?>">
+      <input type="password" id="password" name="password" value="<?= App\Service\Validator::get_input_data('password'); ?>" onchange="this.setAttribute('value', this.value);">
       <label for="password">Password</label>
     </div>
 
@@ -51,7 +51,7 @@
 
   <div class="register-mdp-2">
     <div class="user-box">
-      <input type="password" id="password_confirm" name="password_confirm" value="<?= App\Service\Validator::get_input_data('password_confirm'); ?>">
+      <input type="password" id="password_confirm" name="password_confirm" value="<?= App\Service\Validator::get_input_data('password_confirm'); ?>" onchange="this.setAttribute('value', this.value);">
       <label for="password_confirm">Password Confirm</label>
     </div>
   </div>
