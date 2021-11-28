7cb4349c90277ec63633520cf857f8038ff7ab61
enPausee <78751214+enPausee@users.noreply.github.com>
2021-11-28 18:15:18 +0100

    Revert "Feature order"

--- src/View/footer/faq.php
+++ src/View/footer/faq.php
@@ -2,113 +2,117 @@
 
 ?>
 
-<h1><?=$this->h1?></h1>
+<h1><?= $this->h1 ?></h1>
 <section>
-    <ul>
-        <li><a href="#contact">Comment puis-je vous joindre</a></li>
-        <li><a href="#paiement">Comment puis-je payer ma location</a></li>
-        <li><a href="#consultation">Puis-je consultez les tarifs sans effectuer de reservation</a></li>
-        <li><a href="#annulation">Est il possible de modifier ou annuler ma réservation après avoir recu ma
-                confirmation</a></li>
-        <li><a href="#limite">Existe t'il une limite de kilométrage pour ma location</a></li>
-        <li><a href="#facture">Puis-je obtenir une copie de ma facture</a></li>
-        <li><a href="#pays">Puis-je me rendre dans un autre pays au volant du vehicule</a></li>
-        <li><a href="#depot">Un dépôt de garantie sera t'il prélevé lorsque je retire la voiture</a></li>
-    </ul>
+  <ul>
+    <li><a href="#contact">Comment puis-je vous joindre</a></li>
+    <li><a href="#paiement">Comment puis-je payer ma location</a></li>
+    <li><a href="#consultation">Puis-je consultez les tarifs sans effectuer de reservation</a></li>
+    <li><a href="#annulation">Est il possible de modifier ou annuler ma réservation après avoir recu ma
+        confirmation</a></li>
+    <li><a href="#limite">Existe t'il une limite de kilométrage pour ma location</a></li>
+    <li><a href="#facture">Puis-je obtenir une copie de ma facture</a></li>
+    <li><a href="#pays">Puis-je me rendre dans un autre pays au volant du vehicule</a></li>
+    <li><a href="#depot">Un dépôt de garantie sera t'il prélevé lorsque je retire la voiture</a></li>
+  </ul>
 </section>
 <hr>
 <h3 id="contact">Comment puis je vous joindre</h3>
 <br>
 <div>
-    <p>Vous pouvez nous joindre a cette page</p>
-    <p><a href="/footer/contact">Formulaire de contact</a></p>
+  <p>Vous pouvez nous joindre a cette page</p>
+  <p><a href="/footer/contact">Formulaire de contact</a></p>
 </div>
 <hr>
 <h3 id="paiement">Comment puis je payer ma location</h3>
 <br>
 <div id="paiement">
-    <p>La location sera payée au retour du véhicule, sauf si vous avez effectué une réservation au tarif prépayé (offre
-        Payez en ligne). Dans ce cas, votre carte de crédit sera débitée du montant de la location lors de la
-        réservation.
-        Pour des réservations non prépayées, un dépôt vous sera exigé au moment du retrait et vous devrez effectuer le
-        paiement à la fin de la location.
-        Nous acceptons les moyens de paiement suivants : cartes de crédit, paypal. Cela
-        peut varier selon le type de véhicule que vous louez.</p>
+  <p>La location sera payée au retour du véhicule, sauf si vous avez effectué une réservation au tarif prépayé (offre
+    Payez en ligne). Dans ce cas, votre carte de crédit sera débitée du montant de la location lors de la
+    réservation.
+    Pour des réservations non prépayées, un dépôt vous sera exigé au moment du retrait et vous devrez effectuer le
+    paiement à la fin de la location.
+    Nous acceptons les moyens de paiement suivants : cartes de crédit, paypal. Cela
+    peut varier selon le type de véhicule que vous louez.</p>
 </div>
 <hr>
 <h3 id="consultation">Puis je consultez les tarifs sans effectuez de reservation</h3>
 <br>
 <div id="consultation">
-    <p>Oui. Vous pouvez demander un devis DonkeyCar et vérifier la disponibilité du véhicule sans effectuer de
-        réservation. Remplissez uniquement la page d’accueil sur votre site DonkeyCar et cliquez sur « Obtenir un devis
-        ». Pour sauvegarder ou recevoir un devis, cliquez sur « Détails du véhicule et du tarif ». Vous y trouverez plus
-        de détails concernant le véhicule et le tarif. Si vous souhaitez effectuer une réservation à ce moment-là et
-        avec ces détails, il vous suffit de continuer en cliquant sur le bouton « Réserver »</p>
+  <p>Oui. Vous pouvez demander un devis DonkeyCar et vérifier la disponibilité du véhicule sans effectuer de
+    réservation. Remplissez uniquement la page d’accueil sur votre site DonkeyCar et cliquez sur « Obtenir un devis
+    ». Pour sauvegarder ou recevoir un devis, cliquez sur « Détails du véhicule et du tarif ». Vous y trouverez plus
+    de détails concernant le véhicule et le tarif. Si vous souhaitez effectuer une réservation à ce moment-là et
+    avec ces détails, il vous suffit de continuer en cliquant sur le bouton « Réserver »</p>
 </div>
 <hr>
 <h3 id="annulation">Est il possible de modifier ou annuler ma réservation après avoir recu ma confirmation</h3>
 <br>
 <div>
-    <p>Oui. DonkeyCar vous permet de modifier ou d’annuler votre réservation en ligne après confirmation. L'annulation
-        est gratuite jusqu'à 48h avant le départ. Vous devez simplement cliquer sur le lien « Voir/modifier/annuler une
-        réservation » dans la rubrique « Mon compte ».
-        Si vous n’annulez pas la réservation, que vous ne retirez pas le véhicule avant l’heure de fermeture de
-        l’agence, ou que vous annulez au delà du préavis de 48h, des frais de 50 € ou d’un montant équivalent dans la
-        devise du pays de location vous seront facturés. Veuillez noter que vous pouvez uniquement modifier ou annuler
-        une réservation que vous avez effectuée vous-même en ligne. Vous recevrez une confirmation de la modification ou
-        de l’annulation par e-mail.</p>
+  <p>Oui. DonkeyCar vous permet de modifier ou d’annuler votre réservation en ligne après confirmation. L'annulation
+    est gratuite jusqu'à 48h avant le départ. Vous devez simplement cliquer sur le lien « Voir/modifier/annuler une
+    réservation » dans la rubrique « Mon compte ».
+    Si vous n’annulez pas la réservation, que vous ne retirez pas le véhicule avant l’heure de fermeture de
+    l’agence, ou que vous annulez au delà du préavis de 48h, des frais de 50 € ou d’un montant équivalent dans la
+    devise du pays de location vous seront facturés. Veuillez noter que vous pouvez uniquement modifier ou annuler
+    une réservation que vous avez effectuée vous-même en ligne. Vous recevrez une confirmation de la modification ou
+    de l’annulation par e-mail.</p>
 </div>
 <hr>
 <h3 id="limite">Existe t'il une limite de kilométrage pour ma location</h3>
 <br>
 <div>
-    <p>
-        DonkeyCar propose deux types de tarifs :
-        - Le tarif incluant le kilométrage illimité : vous pouvez conduire autant que vous le souhaitez. Toutefois, nous
-        recommandons qu’aucun conducteur ne soit au volant pendant une durée prolongée et/ou sans se reposer
-        correctement ;
-        - Le tarif incluant un kilométrage limité : nous vous facturerons un supplément pour tout kilométrage dépassant
-        cette limite. Les pages de notre service de réservation en ligne relatives aux tarifs indiquent si le
-        kilométrage illimité est inclus. Vous pouvez également poser la question lors du retrait de votre voiture.
-    </p>
+  <p>
+    DonkeyCar propose deux types de tarifs :
+    - Le tarif incluant le kilométrage illimité : vous pouvez conduire autant que vous le souhaitez. Toutefois, nous
+    recommandons qu’aucun conducteur ne soit au volant pendant une durée prolongée et/ou sans se reposer
+    correctement ;
+    - Le tarif incluant un kilométrage limité : nous vous facturerons un supplément pour tout kilométrage dépassant
+    cette limite. Les pages de notre service de réservation en ligne relatives aux tarifs indiquent si le
+    kilométrage illimité est inclus. Vous pouvez également poser la question lors du retrait de votre voiture.
+  </p>
 </div>
 <hr>
 <h3 id="facture">Puis je obtenir une copie de ma facture</h3>
 <br>
 <div>
-    <p>
-        Vous pouvez obtenir une copie de votre facture en cliquant sur la rubrique « Mon compte », puis sur « Imprimer
-        une
-        facture ».
-        Pour extraire l’information, vous aurez besoin de votre adresse e-mail et de votre mot de passe
-    </p>
+  <p>
+    Vous pouvez obtenir une copie de votre facture en cliquant sur la rubrique « Mon compte », puis sur « Imprimer
+    une
+    facture ».
+    Pour extraire l’information, vous aurez besoin de votre adresse e-mail et de votre mot de passe
+  </p>
 </div>
 <hr>
 <h3 id="pays">Puis je me rendre dans un autre pays au volant du vehicule</h3>
 <br>
 <div>
-    <p>DonkeyCar peut vous permettre de traverser la frontière avec le véhicule. Cela varie en fonction du pays de
-        location, des catégories de véhicules et des pays concernés. Veuillez consulter nos pages relatives aux
-        Conditions générales de vente.</p>
+  <p>DonkeyCar peut vous permettre de traverser la frontière avec le véhicule. Cela varie en fonction du pays de
+    location, des catégories de véhicules et des pays concernés. Veuillez consulter nos pages relatives aux
+    Conditions générales de vente.</p>
 </div>
 <hr>
 <h3 id="depot">Un dépôt de garantie sera t'il prélevé lorsque je retire la voiture</h3>
 <br>
 <div>
-    Nous devons estimer les frais de location de la voiture et obtenir un numéro d’autorisation de votre banque.
-    Cette estimation repose sur :
-    <ul>
-        <li>la durée estimée de location ;</li>
-        <li>le tarif en vigueur, taxes comprises ;</li>
-        <li>les frais imprévus, notamment le carburant.</li>
-    </ul>
-    Ce montant n’est pas débité. Il demeure sur le compte du titulaire de la carte jusqu’au moment où le montant
-    final de la location est débité. Dans certains pays, des exceptions existent toutefois. Pour plus de précisions,
-    veuillez vous reporter aux informations générales relatives à la location. Les dépôts de garantie et le paiement
-    en espèces des locations sont limités au sein du réseau DonkeyCar (là où ils sont acceptés, les dépôts en
-    espèces
-    doivent être accompagnés de formulaires supplémentaires prouvant l’identité – veuillez vérifier ces informations
-    auprès de votre Centre de réservation DonkeyCar)
-    </p>
+  Nous devons estimer les frais de location de la voiture et obtenir un numéro d’autorisation de votre banque.
+  Cette estimation repose sur :
+  <ul>
+    <li>la durée estimée de location ;</li>
+    <li>le tarif en vigueur, taxes comprises ;</li>
+    <li>les frais imprévus, notamment le carburant.</li>
+  </ul>
+  Ce montant n’est pas débité. Il demeure sur le compte du titulaire de la carte jusqu’au moment où le montant
+  final de la location est débité. Dans certains pays, des exceptions existent toutefois. Pour plus de précisions,
+  veuillez vous reporter aux informations générales relatives à la location. Les dépôts de garantie et le paiement
+  en espèces des locations sont limités au sein du réseau DonkeyCar (là où ils sont acceptés, les dépôts en
+  espèces
+  doivent être accompagnés de formulaires supplémentaires prouvant l’identité – veuillez vérifier ces informations
+  auprès de votre Centre de réservation DonkeyCar)
+  </p>
 </div>
 <hr>
+<br>
+<br>
+<br>
+<br>
