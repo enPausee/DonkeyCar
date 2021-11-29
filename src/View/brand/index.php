a66562b706405bba446f42cca8fb3cbf6f87f17b
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2021-11-29 11:13:29 +0100

    removed useless view, img and code and added MainModel

--- src/View/brand/index.php
+++
@@ -1,6 +0,0 @@
-<h1><?= $this->h1 ?></h1>
-<ul>
-  <?php foreach ($brands as $brand) : ?>
-    <li><a href="brand/show/<?= $brand->id ?>"><?= $brand->name ?></a></li>
-  <?php endforeach ?>
-</ul>
--- src/View/category/index.php
+++
@@ -1,8 +0,0 @@
-<h1><?= $this->h1 ?></h1>
-<ul>
-  <?php foreach ($categories as $category) : ?>
-    <li>
-      <?= $category->name ?>
-    </li>
-  <?php endforeach ?>
-</ul>
